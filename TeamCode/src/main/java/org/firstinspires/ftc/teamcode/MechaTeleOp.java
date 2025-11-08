0e7b7e89813dfb5d82e9dd6548504d87b5995e7f
Arjun Ganesan <akhilarjunganesh@gmail.com>
2025-11-08 17:00:54 -0500

    Studio Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechaTeleOp.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Camera;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -10,51 +11,87 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 @TeleOp(name = "MechaTeleOp")
 public class MechaTeleOp extends LinearOpMode {
-    private DcMotor fL, bL, fR, bR;
-    private DcMotor outtake;
-    private Servo lights, carouselRotatator, linkage;
-    private ColorSensor indicator;
-    private float CurrentColor;
+    private DcMotor fL, bL, fR, bR; // carousel rotator will be fR
+    private DcMotor intake, outtake;
+    private Servo lights, linkage;
+    private CRServo carouselRotator;
+    private ColorSensor indicator1, indicator2;
+    private float CurrentColor1, CurrentColor2;
     private final double ticks_in_degree = 700/180.0;
+    private WebcamName cam;
+    private int motif = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
+        cam = hardwareMap.get(WebcamName.class, "cam");
         fL = hardwareMap.dcMotor.get("fL");
         bL = hardwareMap.dcMotor.get("bL");
         fR = hardwareMap.dcMotor.get("fR");
         bR = hardwareMap.dcMotor.get("bR");
+        intake = hardwareMap.dcMotor.get("intake");
+        outtake = hardwareMap.dcMotor.get("outtake");
 
 
+        carouselRotator = hardwareMap.get(CRServo.class, "carouselRotator");
+        linkage = hardwareMap.get(Servo.class, "linkage");
         lights = hardwareMap.get(Servo.class, "lights");
-        indicator = hardwareMap.colorSensor.get("indicator");
+        indicator1 = hardwareMap.colorSensor.get("indicator1");
+        indicator2 = hardwareMap.colorSensor.get("indicator2");
 
 
         fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
+        intake.setDirection(DcMotor.Direction.REVERSE);
         fR.setDirection(DcMotor.Direction.REVERSE);
         bR.setDirection(DcMotor.Direction.REVERSE);
         double maxSpeed = 0.8;
+        double degrees = fR.getCurrentPosition() * (360.0/8192.0);
+
+        while (!isStarted() && !isStopRequested()) {
+
+            // Increase number with D-Pad Up
+            if (gamepad1.dpad_up) {
+                motif++;
+                sleep(200);  // debounce delay
+            } else if (gamepad1.dpad_down) {
+                motif--;
+                sleep(200); //debounce delay
+            }
+
+            telemetry.addData("Motif", motif);
+        }
 
 
         double f, r, s;
         double fLeftPower, bLeftPower, fRightPower, bRightPower;
 
         waitForStart();
-        // April Tags
-        int motifZero = 0, motifOne = 1, motifTwo = 2;
+        // Rising/Falling Edge Detector
+        Gamepad lastGamepad2 = new Gamepad();
+
+        // Initial Positions
+        linkage.setPosition(0);
+        moveToPosition(0);
+
+        // April Tags | Check for the motif and set the rest to zero. | One-GPP; Two-PGP; Three PPG
+        boolean motifOne = false, motifTwo = false;
+        if (motif == 1) {
+            motifOne = true;
+        } else if (motif == 2) {
+            motifTwo = true;
+        }
 
         // Positions of each of section of the carousel | 0-Purple, 1-Green
         int pos1 = 0, pos2 = 0, pos3 = 0;
@@ -64,16 +101,151 @@ public class MechaTeleOp extends LinearOpMode {
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                CurrentColor = JavaUtil.rgbToHue(indicator.red(), indicator.green(), indicator.blue());
-                telemetry.addData("Hue", CurrentColor);
+                CurrentColor1 = JavaUtil.rgbToHue(indicator1.red(), indicator1.green(), indicator1.blue());
+                CurrentColor2 = JavaUtil.rgbToHue(indicator2.red(), indicator2.green(), indicator2.blue());
+                telemetry.addData("Sensor 1 Hue", CurrentColor1);
+                telemetry.addData("Sensor 2 Hue", CurrentColor2);
                 telemetry.update();
-                if (CurrentColor > 240 && CurrentColor < 360) {
+                if (CurrentColor1 > 240 && CurrentColor1 < 360) {
                     lights.setPosition(0.715);
-                } else if (CurrentColor > 60 && CurrentColor < 240) {
+                    pos1 = 0;
+                } else if (CurrentColor1 > 60 && CurrentColor1 < 240) {
                     lights.setPosition(0.5);
+                    pos1 = 1;
                 } else {
                     lights.setPosition(0.279);
                 }
+                if (CurrentColor2 > 240 && CurrentColor2 < 360) {
+                    pos2 = 0;
+                } else if (CurrentColor2 > 60 && CurrentColor2 < 240) {
+                    pos2 = 1;
+                }
+
+//                if (gamepad2.right_bumper) {
+                    // Color Determination Algorithm
+                    // If you have GPP
+//                    if (pos1 == 1 && pos2 == 0) {
+//                        pos3 = 0;
+//                        if (motifOne) {
+//                            autoShoot();
+//                        } else if (motifTwo) {
+//                            outtake.setPower(1);
+//                            carouselRotator.setPosition(0.3);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            shoot();
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            sleep(200);
+//                            outtake.setPower(0);
+//                        } else {
+//                            outtake.setPower(1);
+//                            carouselRotator.setPosition(0.3);
+//                            shoot();
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            shoot();
+//                            sleep(200);
+//                            outtake.setPower(0);
+//
+//                        }
+//                        // If you have PGP
+//                    } else if (pos1 == 0 && pos2 == 1) {
+//                        pos3 = 0;
+//                        if (motifOne) {
+//                            outtake.setPower(1);
+//                            carouselRotator.setPosition(0.3);
+//                            shoot();
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            shoot();
+//                            sleep(200);
+//                            outtake.setPower(0);
+//                        } else if (motifTwo) {
+//                            autoShoot();
+//                        } else {
+//                            outtake.setPower(1);
+//                            shoot();
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0.3);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            sleep(200);
+//                            outtake.setPower(0);
+//                        }
+//                        // If you have PPG
+//                    } else if (pos1 == 0 && pos2 == 0) {
+//                        pos3 = 1;
+//                        if (motifOne) {
+//                            outtake.setPower(1);
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0.3);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            shoot();
+//                            sleep(200);
+//                            outtake.setPower(0);
+//
+//                        } else if (motifTwo) {
+//                            outtake.setPower(1);
+//                            shoot();
+//                            carouselRotator.setPosition(0);
+//                            shoot();
+//                            carouselRotator.setPosition(0.6);
+//                            shoot();
+//                            carouselRotator.setPosition(0.3);
+//                            sleep(200);
+//                            outtake.setPower(0);
+//
+//                        } else {
+//                            autoShoot();
+//                        }
+//                        // Accidental non-pattern
+//                    } else {
+//                        autoShoot();
+//                    }
+//                }
+
+                if (gamepad2.left_bumper) {
+                    outtake.setPower(0.17);
+                }  else if (gamepad2.a) {
+                    outtake.setPower(0.5);
+                } else if (gamepad2.y) {
+                    outtake.setPower(1);
+                } else {
+                    outtake.setPower(0);
+                }
+
+
+                // Manual Methods
+                //Intake
+                if (gamepad1.a) {
+                    intake.setPower(1);
+                } else {
+                    intake.setPower(0);
+
+                }
+                // Linkage Gamepad 2
+//                if (gamepad2.a) {
+//                    linkage.setPosition(0);
+//                } else if (gamepad2.y) {
+//                    linkage.setPosition(0.6);
+//                }
+//                // Carousel Rotator
+                if (gamepad2.x && !lastGamepad2.x) {
+                    moveToPosition(degrees + 120.0);
+                }
+                if (gamepad2.b && !lastGamepad2.b) {
+                    moveToPosition(degrees-120.0);
+                }
+                
+
+
 
                 f = gamepad1.left_stick_y;
                 r = -gamepad1.right_stick_x;
@@ -96,6 +268,26 @@ public class MechaTeleOp extends LinearOpMode {
                 bR.setPower(bRightPower * maxSpeed);
             }
         }
+    }
+
+    private void moveToPosition(double targetDegrees) throws InterruptedException {
+        double kP = 0.005;
+        double currentDegrees = fR.getCurrentPosition()*(360.0/8192.0);
+        double error = targetDegrees-currentDegrees;
+        double power = kP*error;
+        power = Math.max(-1.0, Math.min(1.0, power));
+        if (Math.abs(error) < 2.0) {
+            power = 0;
+        }
+        carouselRotator.setPower(power);
+    }
+
+    private void sort() throws InterruptedException {
+    }
+    private void shoot() throws InterruptedException {
+        linkage.setPosition(0.5);
+        sleep(500);
+        linkage.setPosition(0);
     }
 }
 
